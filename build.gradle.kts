59b339d8a20d5f800d5acf8920592b98d3109f79
Черепахин Василий <vasi.che@gmail.com>
2024-02-07 22:33:25 +0500

    add allure report

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.6"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("io.qameta.allure") version "2.11.0"
 	kotlin("jvm") version "1.8.21"
 	kotlin("plugin.spring") version "1.8.21"
 }
@@ -18,9 +19,13 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation(kotlin("stdlib-jdk8"))
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.rest-assured:rest-assured")
-	implementation(kotlin("stdlib-jdk8"))
+
+	// Allure
+	implementation("io.qameta.allure:allure-junit5:2.19.0")
+	runtimeOnly("org.aspectj:aspectjweaver:1.9.7")
 }
 
 tasks.withType<KotlinCompile> {
@@ -33,6 +38,7 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
 kotlin {
 	jvmToolchain(11)
 }
