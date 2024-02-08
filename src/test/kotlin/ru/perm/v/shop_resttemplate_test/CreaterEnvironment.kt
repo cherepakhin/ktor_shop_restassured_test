981366e2af2df86fb46e07bcd610ea941952462b
Черепахин Василий <vasi.che@gmail.com>
2024-02-08 21:02:35 +0500

    add //TODO: change to real resources (cpu, ram, host, ip, version project, group, artifact)

--- src/test/kotlin/ru/perm/v/shop_resttemplate_test/CreaterEnvironment.kt
+++ src/test/kotlin/ru/perm/v/shop_resttemplate_test/CreaterEnvironment.kt
@@ -8,6 +8,7 @@ import java.nio.file.Path
 class CreaterEnvironment {
     @Test
     fun copyFileEnvironmnet() {
+        //TODO: change to real resources (cpu, ram, host, ip, version project, group, artifact)
         val envSrc = Path.of("src/test/resources/environment.properties")
         val envTarget = Path.of("build/allure-results/environment.properties")
         Files.copy(envSrc, envTarget)
